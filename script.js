46103afc68023d84de742bc15b6fb621a2211e82
nidvar <djayshine87@hotmail.com>
2019-05-09 20:17:10 +1000

    fixed it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const notes = start();
+
 document.querySelector('.enter-title').addEventListener('click', ()=>{
 	add_note_title();
 });
@@ -7,5 +8,24 @@ document.getElementById('user-input').addEventListener('keydown', (e)=>{
 		add_note_title();
 	}
 });
+
+window.addEventListener('storage', (e)=>{
+	if(e.key === 'notes'){
+		document.getElementById('display').innerHTML = '';
+		notes1 = JSON.parse(e.newValue)
+
+	notes1.forEach((a)=>{
+		const x = document.createElement('a');
+		x.setAttribute('href', `edit.html#${a.id}`)
+		x.setAttribute('class', `note-title`)
+		x.setAttribute('id', `${a.id}`)
+		x.textContent = a.title
+		document.getElementById('display').appendChild(x);
+	})
+
+
+
+	}
+})
 
 display_array();
